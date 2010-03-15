b8dd15db1ecba934fb54f74cd90583037fd92d62
Sylvain Viollon <thefunny@gmail.com>
2010-03-15 17:21:46 +0100

    Correct an odd case where you do a del container[relation] of an inexisting relation. You should not get a strange error in that case. Merely a KeyError.

--- src/dolmen/relations/container.py
+++ src/dolmen/relations/container.py
@@ -15,7 +15,7 @@ class RelationsContainer(OOBTree):
     """A rough implementation of a relation storage.
     """
     implements(IRelationsContainer)
-        
+
     def __setitem__(self, key, value):
         checkObject(self, key, value)
         value.__parent__ = self
@@ -25,5 +25,11 @@ class RelationsContainer(OOBTree):
         notify(events.RelationAddedEvent(value))
 
     def __delitem__(self, key):
-        notify(events.RelationDeletedEvent(self.get(key)))
-        OOBTree.__delitem__(self, key)
+        relation = self.get(key, None)
+        if relation is not None:
+            # If the key doesn't exists, nothing should happens. This
+            # case can be triggered if you set a subscribers to
+            # IRelationTargetDeletedEvent that end-up somehow deleting
+            # the relation
+            notify(events.RelationDeletedEvent(self.get(key)))
+            OOBTree.__delitem__(self, key)
